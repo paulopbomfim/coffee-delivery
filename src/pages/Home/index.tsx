940487e8f1832c6cd15991ba73644777818b5a20
Paulo Bomfim <paulo.bomfim@lekto.com.br>
2023-08-20 18:22:24 -0300

    :construction: structure of hero section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,22 @@
 
 export function Home() {
 	return (
-		<div>Home</div>
+		<main>
+			<section>
+				<h1>Encontre o café perfeito para qualquer hora do dia</h1>
+				<span>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora.</span>
+
+				<div>
+					<span>Compra simples e segura</span>
+					<span>Embalagem mantém o café intacto</span>
+					<span>Entrega rápida e rastreada</span>
+					<span>O café chega fresquinho até você</span>
+				</div>
+
+				<img src="" alt="" />
+			</section>
+
+
+		</main>
 	);
 }
