bc767df4cebf69acfaff295b7f080038f406eeea
Paulo Henrique Bomfim <paulo.bomfim@lekto.com.br>
2023-09-02 01:23:53 -0300

    :wrench: chore: setup axios and miragejs

--- src/components/Header/components/GeographicLocation/index.tsx
+++ src/components/Header/components/GeographicLocation/index.tsx
@@ -2,6 +2,8 @@ import { MapPin } from "phosphor-react";
 import { GeographicLocationContainer } from "./styles";
 import { useEffect, useState } from "react";
 
+// import { apiLocation } from "../../../../axios/api";
+
 interface locationType {
 	city: string,
 	principalSubdivisionCode: string
@@ -9,7 +11,7 @@ interface locationType {
 
 export default function GeographicLocation() {
 	const [location, setLocation] = useState({} as locationType);
-
+	
 	let latitude: number;
 	let longitude: number;
 	
@@ -23,6 +25,13 @@ export default function GeographicLocation() {
 	useEffect(() => {
 		const URL = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=pt-BR`;
 		fetch(URL).then((response) =>  response.json()).then((data) => setLocation(data));	
+
+		// apiLocation
+		// .get(`?latitude=${latitude}&longitude=${longitude}&localityLanguage=pt-BR`)
+		// .then((response) =>  setLocation(response.data))
+		// .catch((err) => { console.log("ops! ocorreu um erro" + err); });
+
+
 	}, []);
 
 	return (
